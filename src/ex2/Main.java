f6b357021d35b24c2d161829e9e012f2a9acd33d
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-06-24 15:01:07 +0200

    exercise 2

--- src/ex2/Main.java
+++ src/ex2/Main.java
@@ -1,4 +1,45 @@
 package ex2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class Main {
+    public static void main(String[] args) {
+
+        arrayNumbersOrdinati(5);
+        listaInversa(arrayNumbersOrdinati(5));
+    }
+
+    public static ArrayList arrayNumbersOrdinati(int n) {
+        Random random = new Random();
+        ArrayList<Integer> listaOrdinata = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int nextNum = random.nextInt(0, 100);
+            listaOrdinata.add(nextNum);
+        }
+
+        Collections.sort(listaOrdinata);
+        System.out.println(listaOrdinata);
+        return listaOrdinata;
+
+    }
+
+    public static ArrayList listaInversa(ArrayList lista) {
+        ArrayList<Integer> nuovaLista = new ArrayList<>();
+
+        for (int i = 0; i < lista.size(); i++) {
+            nuovaLista.add((Integer) lista.get(i));
+        }
+
+        for (int i = lista.size(); i >= 0; i--) {
+            int eleToAdd = (Integer) lista.get(i);
+            nuovaLista.add(eleToAdd);
+        }
+
+        System.out.println(nuovaLista);
+        return nuovaLista;
+    }
+
+
 }
